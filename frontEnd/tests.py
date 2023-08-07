bc412ea048783b3595379f606884fc265676e6b8
Helenka Casler <casler.helenka@gmail.com>
2023-08-06 20:07:26 -0400

    added unit tests for nonexistent resources

--- frontEnd/tests.py
+++ frontEnd/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from django.contrib.staticfiles.storage import staticfiles_storage
 
 # Create your tests here.
+
+class NonexistentResourceTests(TestCase):
+
+    def testNonExistentMainPage(self):
+        response = self.client.get("/thisPageDoesNotExist")
+        self.assertEqual(response.status_code, 404)
+        # should return the top-level nonexistent page
+        self.assertIn('<div id="navbar">', str(response.content))
+
+
+    def testNonExistentSubPage(self):
+        response = self.client.get("/contentPages/thisPageDoesNotExist")
+        self.assertEqual(response.status_code, 404)
+        # this should be a sub-page, not a complete document
+        self.assertNotIn('<div id="navbar">', str(response.content))
+
+    def testNonExistentStaticFile(self):
+        response = self.client.get(staticfiles_storage.url('frontEnd/images/noSuchImageHere.jpg'))
+        self.assertEqual(response.status_code, 404)
+
