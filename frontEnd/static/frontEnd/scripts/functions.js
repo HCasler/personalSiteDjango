d9cfceb2fbdf1babd6255c06426d603f23d949b2
Helenka Casler <casler.helenka@gmail.com>
2023-08-09 15:08:05 -0400

    displayPageContent treats everything as a subpage and never tries to load a remote site

--- frontEnd/static/frontEnd/scripts/functions.js
+++ frontEnd/static/frontEnd/scripts/functions.js
@@ -1,6 +1,10 @@
 // function to bring up the content when clicking a link
 
 async function displayPageContent(subpage) {
+    // strip off any http or https part, then the site
+    // will try to treat it as a subPage
+    subpage = subpage.replace('http://', '')
+    subpage = subpage.replace('https://', '')
     // retrieve the content
     const response = await fetch(subpage);
     const newText = await response.text();
