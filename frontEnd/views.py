d06381003420db74e3b8a88ce350647e369b8d1d
Helenka Casler <casler.helenka@gmail.com>
2023-08-10 15:52:31 -0400

    cleaned up views a little bit, updated 404 page

--- frontEnd/views.py
+++ frontEnd/views.py
@@ -4,6 +4,15 @@ from django.template.exceptions import TemplateDoesNotExist
 
 logger = logging.getLogger(__name__)
 
+def returnSubPageOr404(request, templatePath, context):
+    try:
+        return render(request, templatePath, context)
+    except TemplateDoesNotExist:
+        logger.error("Failed to retrieve template {0}".format(templatePath))
+        return nonexistentSubpage(request, request.path)
+
+# ~~~~ actual views start here ~~~~ #
+
 def index(request):
     logger.debug("received {0} request for index page".format(request.method))
     try: 
@@ -14,55 +23,33 @@ def index(request):
 
 def aboutMe(request):
     logger.debug("received {0} request for about-me page".format(request.method))
-    try:
-        return render(request, "frontEnd/contentPages/aboutMe.html", {})
-    except TemplateDoesNotExist:
-        logger.error("Failed to retrieve template frontEnd/contentPages/aboutMe.html")
-        return nonexistentSubpage(request, 'frontEnd/contentPages/aboutMe.html')
+    return returnSubPageOr404(request, "frontEnd/contentPages/aboutMe.html", {})
 
 def coding(request):
     logger.debug("received {0} request for coding page".format(request.method))
-    try:
-        return render(request, "frontEnd/contentPages/coding.html", {})
-    except TemplateDoesNotExist:
-        logger.error("Failed to retrieve template frontEnd/contentPages/coding.html")
-        return nonexistentSubpage(request, 'frontEnd/contentPages/coding.html')
+    return returnSubPageOr404(request, "frontEnd/contentPages/coding.html", {})
 
 def research(request):
     logger.debug("received {0} request for research page".format(request.method))
-    try:
-        return render(request, "frontEnd/contentPages/research.html", {})
-    except TemplateDoesNotExist:
-        logger.error("Failed to retrieve template frontEnd/contentPages/research.html")
-        return nonexistentSubpage(request, 'frontEnd/contentPages/research.html')
+    return returnSubPageOr404(request, "frontEnd/contentPages/research.html", {})
 
 def publications(request):
     logger.debug("received {0} request for publications page".format(request.method))
-    try:
-        return render(request, "frontEnd/contentPages/publications.html", {})
-    except TemplateDoesNotExist:
-        logger.error("Failed to retrieve template frontEnd/contentPages/publications.html")
-        return nonexistentSubpage(request, 'frontEnd/contentPages/publications.html')
+    return returnSubPageOr404(request, "frontEnd/contentPages/publications.html", {})
 
 def contact(request):
     logger.debug("received {0} request for contact page".format(request.method))
-    try:
-        return render(request, "frontEnd/contentPages/contact.html", {})
-    except TemplateDoesNotExist:
-        logger.error("Failed to retrieve template frontEnd/contentPages/contact.html")
-        return nonexistentSubpage(request, 'frontEnd/contentPages/contact.html')
+    return returnSubPageOr404(request, "frontEnd/contentPages/contact.html", {})
 
 def comingSoon(request):
     logger.debug("received {0} request for coming-soon page".format(request.method))
-    try:
-        return render(request, "frontEnd/contentPages/comingSoon.html", {})
-    except TemplateDoesNotExist:
-        logger.error("Failed to retrieve template frontEnd/contentPages/comingSoon.html")
-        return nonexistentSubpage(request, 'frontEnd/contentPages/comingSoon.html')
+    return returnSubPageOr404(request, "frontEnd/contentPages/cominSoon.html", {})
 
-def nonexistentSubpage(request, other_path):
+def nonexistentSubpage(request, other_path=None):
     logger.error("received {0} request for nonexistent path {1}".format(request.method, other_path))
-    resp = render(request, "frontEnd/contentPages/nonexistentSubpage.html", {})
+    context = {"missingThing": request.path}
+    logger.error(context)
+    resp = render(request, "frontEnd/contentPages/nonexistentSubpage.html", context)
     resp.status_code = 404
     return resp
 
