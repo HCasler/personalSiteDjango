3f78aa3af933337714c3d4100d3b5ff5333b1551
Helenka Casler <casler.helenka@gmail.com>
2023-09-15 15:01:50 -0400

    new model for main site content

--- frontEnd/models.py
+++ frontEnd/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class PageText(models.Model):
+    name = models.CharField(max_length=254, unique=True)
+    lastUpdated = models.DateTimeField(max_length=60000)
+    text = models.TextField()
