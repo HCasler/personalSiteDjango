83ea38c6ec5b0ff214a02d2d397b08b0ae4d4ed1
Helenka Casler <casler.helenka@gmail.com>
2023-09-21 22:43:58 -0400

    change title of content authoring site

--- frontEnd/admin.py
+++ frontEnd/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import PageText
 
+# style stuff
+admin.site.site_header = "Content Authoring"
+admin.site.site_title = "Content Authoring"
+
 
 admin.site.register(PageText)
